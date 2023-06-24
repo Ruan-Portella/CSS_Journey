5f9a08c55b6061fb2c0a9789324f2616c7f93a80
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-24 13:25:43 -0300

    Feat: Configurando Babel e Css Loaders

--- css_modules/webpack/webpack.config.js
+++ css_modules/webpack/webpack.config.js
@@ -13,5 +13,25 @@ module.exports = {
         },
         compress: true,
         port: 9000,
-      },
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ]
+      }
 };
